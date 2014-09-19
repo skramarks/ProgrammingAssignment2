f2f0ee4f33c7a391567b3df0547a9b1887242832
skramarks <shoppingsteve@gmail.com>
2014-09-18 21:37:19 -0400

    Works now
    1) Gave variables meaningful names
    2) Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,100 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# This "class" like collection of functions are used efficiently calcuate the inverse of
+# a matrix by calculating it once and storing the result for future use.
+#
+# The function makeCacheMatrix creates the object.
+# The function cacheSolve calculates the inverse of the matrix if nexessary and returns it.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+makeCacheMatrix <- function(originalMatrix = matrix()) {
+    # 
+    # Creates a "CacheMatrix" object that is used to  store the original matrix and a 
+    # transformed version of it.
+    #
+    # Parameter:
+    #
+    #   originalMatrix  The matrix to be stored for transform
+    #
+    #
+    #   Usage example:
+    #       mm <-matrix(c(1, 0, 1, 2,  4,0, 3, 5, 6), 3, 3)
+    #       mmm <- makeCacheMatrix(mm)
+    #       cacheSolve(mmm)
+    
+    
+    
+    # initially no transformed matrix
+    transformedMatrix <- NULL
+
+    # Store a matrix.
+    setOriginal <- function(original) {
+        
+        # store these values in the parent context (that of the object).
+        originalMatrix <<- original
+        
+        # erase an old transformed matrix
+        transformedMatrix <<- NULL
     }
-    get <- function() x
-    setinverse <- function(solve) m <<- solve
-    getinverse <- function() m
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
+    
+    
+    # fetch the stored matrix
+    getOriginal <- function() originalMatrix
+    
+    
+    #
+    # Stores the transformed matrix in the object.
+    #
+    setTransformed <- function(transformed) transformedMatrix <<- transformed
+    
+    #
+    # Returns the stored transformed matrix
+    #
+    # The return value will be NULL if it has not been set
+    #
+    getTransformed <- function() transformedMatrix
+    
+    
+    #
+    # create and return the "object"
+    #
+    list(setOriginal = setOriginal, getOriginal = getOriginal,
+         setTransformed = setTransformed,
+         getTransformed = getTransformed)
 }
 
 
-## Write a short comment describing this function
-
+#
+#
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    m <- x$getinverse()
-    if(!is.null(m)) {
+    # 
+    # Returns the inverse of a matrix.   If the inverse was not previosly 
+    # calculated, it is calculated and stored in the object for reuse.
+    #
+    # Parameter:
+    #
+    #   x   an object created by makeCacheMatrix
+    #
+    #   Returns:  the inverse of the original matrix
+    #
+    
+    invertedMatrix <- x$getTransformed()
+    
+    if(!is.null(invertedMatrix)) {
+        #
+        # The inverse was already calcualted, return it.
+        #
         message("getting cached data")
-        return(m)
+        return(invertedMatrix)
     }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinverse(m)
-    m
+    
+    # Calculate the inverse
+    data <- x$getOriginal()
+    invertedMatrix <- solve(data, ...)
+    
+    # cache the result
+    x$setTransformed(invertedMatrix)
+    
+    # return a copy of the inverted matrix
+    invertedMatrix
 }
